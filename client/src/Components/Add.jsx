7551e7395fb9017f369d15b48d076afbc363b26e
Michelle-Gn <michelle.guan93@gmail.com>
2021-12-24 19:35:18 -0800

    Implement add modal

--- client/src/Components/Add.jsx
+++ client/src/Components/Add.jsx
@@ -7,12 +7,53 @@ class Add extends React.Component {
 
     }
 
-  }
+    render() {
+      return (
+        <>
+          <button className="button" onClick={handleShow}>
+            Add
+          </button>
 
-  render() {
-    return (
-       <button className="button">Add</button>
-    )
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Entry</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>{`${props.info.country} is a country in ${props.info.subregion} with a population of ${props.info.people}. Its capital is ${props.info.capital} and its currency is the ${props.info.currency}. Hope we can take a trip there someday!`}</div>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                  onChange={
+                    (e) => {setNumber(e.target.value)}
+                  }
+                  value={this.state.employeeName}
+                  placeholder='Employee name (ex. John Doe)'/>
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control
+                  onChange={
+                    (e) => {this.setState({
+                      value: 0
+                    })}
+                  value={this.state.value}
+                  placeholder='Value (ex. 78000)'
+                  }/>
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <button onClick={(e) => {
+                e.preventDefault()
+                sendMessage(props.info.country, props.info.capital, props.info.subregion, props.info.currency, props.info.people, phoneNumber)
+                .then((result) => console.log('message sent!'))
+                .catch((error) => console.log(error))
+              }}>Send!</button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      );
+    }
+
   }
 }
 
